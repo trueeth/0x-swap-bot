05f68b6e63c1ee682a5fd5125c7085632d3d590e
trueeth <wangqianjin26@gmail.com>
2024-03-29 13:46:35 -0700

    fix: remove mongoose error

--- src/modules/errors/error.ts
+++ src/modules/errors/error.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Request, Response, NextFunction } from 'express';
-import mongoose from 'mongoose';
 import httpStatus from 'http-status';
 import config from '../../config/config';
 import { logger } from '../logger';
@@ -9,8 +8,7 @@ import ApiError from './ApiError';
 export const errorConverter = (err: any, _req: Request, _res: Response, next: NextFunction) => {
   let error = err;
   if (!(error instanceof ApiError)) {
-    const statusCode =
-      error.statusCode || error instanceof mongoose.Error ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR;
+    const statusCode = error.statusCode
     const message: string = error.message || `${httpStatus[statusCode]}`;
     error = new ApiError(statusCode, message, false, err.stack);
   }
